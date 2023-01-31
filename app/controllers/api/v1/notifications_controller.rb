0f3243e214d744b6f6879cc3f4dd631a3d51c30d
Turan Sadigova <sadigova.turan@gmail.comm>
2023-01-31 10:40:56 +0400

    Set up MVC for notifications

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -10,7 +10,14 @@ class Api::V1::NotificationsController < ApplicationController
                            size: '10',
                            cursor: {
                             nav_dir: 'after',
-                            pointer: '10'
+                            pointer: '-1'
                            }).permit([:order, :size, cursor: [:nav_dir, :pointer]])
+    end
+
+    def prev_page_link_params(pointer)
+      params.merge(cursor: {
+        nav_dir: 'before',
+        pointer: '12'
+      })
     end
 end
