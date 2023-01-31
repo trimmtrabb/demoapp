04888d521102b89e2a9fe25e19155290280fb227
Turan Sadigova <sadigova.turan@gmail.comm>
2023-01-31 10:39:02 +0400

    Create table 'notifications' & add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+(0...1000).step(10).each do |i|
+  FactoryBot.create(:notification,
+                    created_at: Time.current + i.minutes,
+                    updated_at: Time.current + i.minutes)
+end
