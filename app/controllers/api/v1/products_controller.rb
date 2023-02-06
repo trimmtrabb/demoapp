fe2d306d7670e64536d8d43c7d244dcd7048a981
Turan Sadigova <sadigova.turan@gmail.comm>
2023-02-06 16:46:13 +0400

    API: Add action #create to accept form data including video attachments

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::ProductsController < Api::ApplicationController
+  def create
+    @product = Product.new(product_params)
+    # binding.break
+    if @product.save
+      render json: @product, status: :created #, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :description, videos: [])
